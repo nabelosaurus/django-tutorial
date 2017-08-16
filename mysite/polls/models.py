e5f439adf825c6d38ab4499cc222e394f4f8aa81
nabelosaurus <nabelosaurus@users.noreply.github.com>
2017-08-16 05:51:42 +0300

    Create models for polls app, plug polls app in settings, run migrations

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date_published')
+    # class Meta:
+    #     verbose_name = "Question"
+    #     verbose_name_plural = "Questions"
+
+    # def __str__(self):
+    #     pass
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    # class Meta:
+    #     verbose_name = "Choice"
+    #     verbose_name_plural = "Choices"
+
+    # def __str__(self):
+    #     pass
