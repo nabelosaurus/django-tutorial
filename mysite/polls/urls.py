ddab254c513ed3325ef95e1f2810f128b8ed34a1
nabelosaurus <nabelosaurus@users.noreply.github.com>
2017-08-20 15:19:02 +0300

    Add skeleton functions for detail, results, and vote views. Wire urls in polls/urls.py to created functions

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # Example: /polls/
     url(r'^$', views.index, name='index'),
+    # Example: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # Example: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # Example: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
