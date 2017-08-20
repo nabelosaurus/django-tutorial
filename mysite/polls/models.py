b9839baf93ac9358e0ad49a11517989c82743746
nabelosaurus <nabelosaurus@users.noreply.github.com>
2017-08-20 03:06:08 +0300

    Create custom method was_published_recently for Question model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,23 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
-    # class Meta:
-    #     verbose_name = "Question"
-    #     verbose_name_plural = "Questions"
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
-    # class Meta:
-    #     verbose_name = "Choice"
-    #     verbose_name_plural = "Choices"
 
     def __str__(self):
         return self.choice_text
