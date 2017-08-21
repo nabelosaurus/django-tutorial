b655ac22d16b6dbc8b4be1d230835c207cd0c21c
nabelosaurus <nabelosaurus@users.noreply.github.com>
2017-08-21 15:34:53 +0300

    Make Index, Detail, and Results -views generic. Re-route urls to the generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,11 +5,15 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # Example: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$',
+        views.IndexView.as_view(), name='index'),
     # Example: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$',
+        views.DetailView.as_view(), name='detail'),
     # Example: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$',
+        views.ResultsView.as_view(), name='results'),
     # Example: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$',
+        views.vote, name='vote'),
 ]
